911ef2df0548ab9de84ecd7bea1fa8570cf8bc6c
Yamada Masaki <masaki@hisme.net>
2010-04-09 03:17:12 +0900

    fix form_for

--- lib/acts_as_opensocial/helpers.rb
+++ lib/acts_as_opensocial/helpers.rb
@@ -1,2 +1,2 @@
 require 'acts_as_opensocial/helpers/url_helper'
-
+require 'acts_as_opensocial/helpers/form_tag_helper'
--- lib/acts_as_opensocial/helpers/form_tag_helper.rb
+++ lib/acts_as_opensocial/helpers/form_tag_helper.rb
@@ -3,8 +3,11 @@ module ActionView
     module FormTagHelper      
       def form_tag_html(html_options)
         extra_tags = extra_tags_for_form(html_options) 
-        case opensocial = html_options.delete('os')
-          when :mixi_mobile
+        case controller.opensocial_type
+        when :mixi_mobile
+          extra_tags += mixi_tags_for_form(html_options)
+          html_options['action'] = '?guid=ON'
+        when true
           extra_tags += mixi_tags_for_form(html_options)
           html_options['action'] = '?guid=ON'
         end
@@ -13,8 +16,15 @@ module ActionView
       
       def mixi_tags_for_form(html_options)
         require 'cgi'
-        path = request.protocol + request.host_with_port + html_options['action']
-        tag(:input, :type => 'hidden', :name => 'url', :value => path)
+        if !html_options['action'].match(/^http:\/\//)
+          path = request.protocol + 
+            request.host_with_port + 
+            html_options['action']
+        else
+          path = html_options['action']
+        end
+        tag(:input, :type => 'hidden', :name => 'url', :value => path) +
+          tag(:input, :type => 'hidden', :name => 'guid', :value => 'ON')
       end
     end
   end
