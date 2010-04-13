7624b16978a2ab67f0dfbc3857e0fd364ce3ff74
yamada masaki <masaki@hisme.net>
2010-04-12 20:35:44 -0700

    fix

--- lib/acts_as_opensocial/filter.rb
+++ lib/acts_as_opensocial/filter.rb
@@ -1,10 +1,14 @@
 class ActionController::Base
-  @@__opensocial_type = false
+  @__opensocial_type = false
   def self.opensocial_filter(type = true)
-    @@__opensocial_type = type
+    @__opensocial_type = type
   end
   
+  def opensocial_filter(type = true)
+     @__opensocial_type = type
+  end  
+  
   def opensocial_type
-    @@__opensocial_type
+    @__opensocial_type
   end
 end
