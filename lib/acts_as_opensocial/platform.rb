f8b8787792e635731d5ea4de062ebba123e5e88c
Yamada Masaki <masaki@hisme.net>
2010-06-04 13:39:24 +0900

    add dummy platform to test local

--- lib/acts_as_opensocial/platform.rb
+++ lib/acts_as_opensocial/platform.rb
@@ -1,3 +1,4 @@
 require 'acts_as_opensocial/platform/abstract_platform'
+require 'acts_as_opensocial/platform/dummy_platform'
 #require 'acts_as_opensocial/platform/mixi'
 #require 'acts_as_opensocial/platform/gree'
