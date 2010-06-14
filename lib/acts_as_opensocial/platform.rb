f29125f85330dd4225e610c618e5a9a773bd6385
Yamada Masaki <masaki@hisme.net>
2010-06-14 17:09:54 +0900

    fix

--- lib/acts_as_opensocial/platform.rb
+++ lib/acts_as_opensocial/platform.rb
@@ -2,6 +2,6 @@ require 'acts_as_opensocial/platform/abstract_platform'
 require 'acts_as_opensocial/platform/dummy_platform'
 require 'acts_as_opensocial/platform/text_api'
 
-ActiveRecord::Base.send(:extend, OpenSocial::TextApi::ActiveRecordExtend::ActMethos)
+ActiveRecord::Base.send(:extend, OpenSocial::TextApi::ActiveRecordExtend::ActMethods)
 #require 'acts_as_opensocial/platform/mixi'
 #require 'acts_as_opensocial/platform/gree'
