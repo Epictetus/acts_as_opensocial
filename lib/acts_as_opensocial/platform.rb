e0d24cdcccb4174a35b6685628bf176601debc1a
Yamada Masaki <masaki@hisme.net>
2010-06-14 16:55:48 +0900

    text_apiのロード

--- lib/acts_as_opensocial/platform.rb
+++ lib/acts_as_opensocial/platform.rb
@@ -1,4 +1,7 @@
 require 'acts_as_opensocial/platform/abstract_platform'
 require 'acts_as_opensocial/platform/dummy_platform'
+require 'acts_as_opensocial/platform/text_api'
+
+ActiveRecord::Base.send(:extend, OpenSocial::TextApi::ActiveRecordExtend::ActMethos)
 #require 'acts_as_opensocial/platform/mixi'
 #require 'acts_as_opensocial/platform/gree'
